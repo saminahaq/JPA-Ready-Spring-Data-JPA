a775f11384e96de8b75c140e00c245d2871274d8
Samina-Haq <Saminan-Haq@localhost>
2022-06-21 22:50:00 -0700

    version1

---
+++ Ready-Spring-Data-JPA/src/test/java/com/SpringDataJPA/ReadySpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadySpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d1e9336903414fe9d4b1c36db2c2dd612c8e8c82
Samina-Haq <Saminan-Haq@localhost>
2022-06-22 18:05:49 -0700

    version 01

---
+++ JPAReady/src/main/java/com/JPAReady/JpaReadyApplication.java
@@ -0,0 +1,13 @@
+package com.JPAReady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaReadyApplication.class, args);
+	}
+
+}
---
+++ JPAReady/src/test/java/com/JPAReady/JpaReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JPAReady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
